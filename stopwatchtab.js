ba3b364a3a42df5975aa43fa425829af17b057c5
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-06-10 10:38:41 -0700

    Further beautify stopwatchtab.js

--- stopwatchtab.js
+++ stopwatchtab.js
@@ -1,16 +1,17 @@
-javascript:(function(){
-  var i, s = 0, d = document.title;
-  function c(e){
-    return (e - (e %= 60)) / 60 + (9 < e?":":":0") + e
-  }
-  
-  function t(){
-    if (s<3600) {
-      s++;
-      document.getElementsByTagName("title")[0].innerHTML = '(' + c(s) + ') ' + d;
-    }
-    else clearInterval(i);
-  }
+javascript: (function() {
+    var i, s = 0,
+        d = document.title;
 
-  i = setInterval(t, 1000);
+    function c(e) {
+        return (e - (e %= 60)) / 60 + (9 < e ? ":" : ":0") + e
+    }
+
+    function t() {
+        if (s < 3600) {
+            s++;
+            document.getElementsByTagName("title")[0].innerHTML = '(' + c(s) + ') ' + d;
+        } else clearInterval(i);
+    }
+
+    i = setInterval(t, 1000);
 })();
