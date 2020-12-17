af900fc80b9fe6ed26993a3ebb26d0e0ca3fa4c7
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-12-17 12:11:07 -0800

    Minor refactor for v1.1.1

--- stopwatchtab.js
+++ stopwatchtab.js
@@ -1,12 +1,12 @@
 javascript:(() => {
     let interval, seconds = 0, initialDocumentTitle = document.title;
 
-    formatSeconds = secondsTotal => new Date(secondsTotal * 1000).toISOString().substr(11, 8);
+    formatSeconds = secondsTotal => new Date(secondsTotal * 1000).toJSON().substr(11, 8);
 
     stopwatch = () => {
         if (seconds < 86400) {
             seconds++;
-            document.querySelector('title').innerHTML = `(${formatSeconds(seconds)}) ${initialDocumentTitle}`;
+            document.title = `(${formatSeconds(seconds)}) ${initialDocumentTitle}`;
         } else clearInterval(interval)
     }
     
