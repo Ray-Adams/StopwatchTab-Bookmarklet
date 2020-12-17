86c422899d85cfe3717daa4c243085012b990b07
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2020-12-17 12:02:08 -0800

    Fix global variable leak and shave off 41 bytes

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:((e,t,n)=>{t=0,n=document.title,d=(k=>{86400>t?(t++,document.querySelector('title').innerHTML=`(${new Date(1e3*t).toISOString().substr(11,8)}) ${n}`):clearInterval(e)}),e=setInterval(d,1e3)})()
+javascript:((a,b,c=0,d=document,e=d.title)=>{a=_=>86400>c?(c++,d.title=`(${new Date(1e3*c).toJSON().substr(11,8)}) ${e}`):clearInterval(b),b=setInterval(a,1e3)})()
